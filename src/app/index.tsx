187c95e3f082c80a0b2630cd33d9b2a658831815
nocollar <nocollar.espresso@zohomail.jp>
2025-01-21 16:22:24 +0900

    20240121

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,11 +1,59 @@
-import { View,Text } from 'react-native'
+import { View,Text,StyleSheet } from 'react-native'
 
 const Index = (): JSX.Element => {
     return (
-        <View>
-            <Text>Hello World</Text>
+        <View style={styles.container}>
+            <View>
+                <View>
+                    <Text>MemoApp</Text>
+                    <Text>ログアウト</Text>
+                </View>
+            </View>
+            <View>
+                <View>
+                    <View>
+                        <Text>買い物リスト</Text>
+                        <Text>何年何月何日何時</Text>
+                    </View>
+                    <View>
+                        <Text>X</Text>
+                    </View>
+                </View>
+
+                <View>
+                    <View>
+                        <Text>買い物リスト</Text>
+                        <Text>何年何月何日何時</Text>
+                    </View>
+                    <View>
+                        <Text>X</Text>
+                    </View>
+                </View>
+
+                <View>
+                    <View>
+                        <Text>買い物リスト</Text>
+                        <Text>何年何月何日何時</Text>
+                    </View>
+                    <View>
+                        <Text>X</Text>
+                    </View>
+                </View>
+
+            </View>
+            <View>
+                <Text>+</Text>
+            </View>
         </View>
     )
 }
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center'
+    }
+})
 
 export default Index
