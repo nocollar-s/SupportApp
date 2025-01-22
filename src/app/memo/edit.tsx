7f4b9743c0c1022be288c74a652ab8bc4208efbb
nocollar <nocollar.espresso@zohomail.jp>
2025-01-22 16:52:05 +0900

    202501221652,edit

---
+++ src/app/memo/edit.tsx
@@ -0,0 +1,39 @@
+import { View, TextInput, StyleSheet } from 'react-native'
+
+import Header from '../../components/Header'
+import CircleButton from '../../components/CircleButton'
+import { Feather } from '@expo/vector-icons'
+
+const Edit = ():JSX.Element => {
+    return (
+        <View style={styles.container}>
+            <Header />
+            <View style={styles.inputContainer}>
+                <TextInput multiline style={styles.input} value={'買い物\nリスト'} />
+            </View>
+            <CircleButton>
+                <Feather name='check' size={40} color='#ffffff' />
+            </CircleButton>
+        </View>
+    )
+}
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        backgroundColor:'#ffffff'
+    },
+    inputContainer:{
+        paddingVertical: 32,
+        paddingHorizontal: 27,
+        flex :1
+    },
+    input:{
+        flex :1, 
+        textAlignVertical: 'top',
+        fontSize: 16,
+        lineHeight: 24
+    }
+})
+
+export default Edit 
