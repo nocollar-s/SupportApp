185e02fe9c417f7321dfb0ca919b264b271fe740
nocollar <nocollar.espresso@zohomail.jp>
2025-01-22 14:03:03 +0900

    202501221403,memoBody

---
+++ src/app/memo/detail.tsx
@@ -0,0 +1,58 @@
+import { View, Text, ScrollView, StyleSheet } from 'react-native'
+
+import Header from '../../components/Header'
+import CircleButton from '../../components/CircleButton'
+
+const Detail = ():JSX.Element => {
+    return (
+        <View style={styles.container}>
+            <Header />
+            <View style={styles.memoHeader}>
+                <Text style={styles.memoTitle}>買い物リスト</Text>
+                <Text style={styles.memoDate}>日付時刻</Text>
+            </View>
+            <ScrollView style={styles.memoBody}>
+                <Text style={styles.memoBodyText}>
+                    何々何々何々
+                </Text>
+            </ScrollView>
+            <CircleButton style={{ top:160, bottom:'auto'}}>+</CircleButton>
+        </View>
+    )
+}
+
+const styles = StyleSheet.create({
+    container:{
+        flex: 1,
+        backgroundColor: '#ffffff'
+    },
+    memoHeader:{
+        backgroundColor: '#467FD3',
+        height: 96,
+        justifyContent: 'center',
+        paddingVertical: 24,
+        paddingHorizontal: 19,
+    },
+    memoTitle:{
+        color:'#ffffff',
+        fontSize: 20,
+        lineHeight: 32,
+        fontWeight:'bold'
+    },
+    memoDate:{
+        color:'#ffffff',
+        fontSize: 12,
+        lineHeight: 16,
+    },
+    memoBody:{
+        paddingVertical: 32,
+        paddingHorizontal: 27,
+    },
+    memoBodyText:{
+        fontSize: 16,
+        lineHeight: 24,
+        color:'#000000',
+    }
+})
+
+export default Detail
