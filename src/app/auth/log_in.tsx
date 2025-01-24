0902d7ebb13d49724d610513d8e0b89df9a1e95a
nocollar <nocollar.espresso@zohomail.jp>
2025-01-24 13:47:56 +0900

    login sign up

--- src/app/auth/log_in.tsx
+++ src/app/auth/log_in.tsx
@@ -4,7 +4,7 @@ import {
 } from 'react-native'
 
 import{ Link, router } from 'expo-router'
-
+import{ useState } from 'react'
 import Button from'../../components/Button'
 
 const handlePresss=(): void => {
@@ -14,13 +14,39 @@ const handlePresss=(): void => {
 }
 
 const LogIn =():JSX.Element =>{
+    const [email, setEmail] = useState('')
+    const [password, setPassword] = useState('')
     return (
         <View style={styles.container}>
 
             <View style={styles.inner}>
                 <Text style={styles.title}>Log In</Text>
-                <TextInput style={styles.input}value='Email address' />
-                <TextInput style={styles.input}value='Password' />
+                <TextInput
+                 style={styles.input}
+                 value={email}
+                 onChangeText={(text) => { setEmail(text)}}
+                 //1文字目が大文字にならない設定//
+                 autoCapitalize='none' 
+                 //入力欄に＠表示
+                 keyboardType = 'email-address'
+                 //入力欄にグレーで文字表示
+                 placeholder='Email Adress'
+                 //先々でキーチェーンに自動保管してもらうかもなので設定しておく
+                 textContentType='emailAddress'
+
+                 />
+                <TextInput
+                style={styles.input}
+                value={password}
+                onChangeText={(text) => { setPassword(text)}}
+                //1文字目が大文字にならない設定//
+                autoCapitalize='none'
+                //パスワード入力中、伏せ字になる
+                secureTextEntry={true}
+                placeholder='Password'
+                //先々でキーチェーンに自動保管してもらうかもなので設定しておく
+                textContentType='password'
+                />
                 <Button label='Submit' onPress={handlePresss} />
                 <View style={styles.footer}>
                     <Text style={styles.footerText}>Not registerd?</Text>
--- src/app/auth/sign_up.tsx
+++ src/app/auth/sign_up.tsx
@@ -3,6 +3,7 @@ import {
     TouchableOpacity, StyleSheet 
 } from 'react-native'
 import {Link, router } from 'expo-router'
+import { useState } from 'react'
 
 import Button from'../../components/Button'
 
@@ -12,12 +13,38 @@ const handlePress = (): void => {
 }
 
 const SignUp =():JSX.Element =>{
+    const [email, setEmail] = useState('')
+    const [password, setPassword] = useState('')
     return (
         <View style={styles.container}>
             <View style={styles.inner}>
                 <Text style={styles.title}>Sign Up</Text>
-                <TextInput style={styles.input}value='Email address' />
-                <TextInput style={styles.input}value='Password' />
+                <TextInput
+                    style={styles.input}
+                    value={email}
+                    onChangeText={(text) => { setEmail(text)}}
+                    //1文字目が大文字にならない設定//
+                    autoCapitalize='none' 
+                    //入力欄に＠表示
+                    keyboardType = 'email-address'
+                    //入力欄にグレーで文字表示
+                    placeholder='Email Adress'
+                    //先々でキーチェーンに自動保管してもらうかもなので設定しておく
+                    textContentType='emailAddress'
+    
+                    />
+                    <TextInput
+                    style={styles.input}
+                    value={password}
+                    onChangeText={(text) => { setPassword(text)}}
+                    //1文字目が大文字にならない設定//
+                    autoCapitalize='none'
+                    //パスワード入力中、伏せ字になる
+                    secureTextEntry={true}
+                    placeholder='Password'
+                    //先々でキーチェーンに自動保管してもらうかもなので設定しておく
+                    textContentType='password'
+                    />
                 <Button label='Submit' onPress={handlePress}  />
                 <View style={styles.footer}>
                     <Text style={styles.footerText}>Already registered?</Text>
